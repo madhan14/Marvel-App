81c30bb4cc1701c9df020a36d20a39c980049463
madhan14 <madhankumar05072000@gmail.com>
2023-01-02 11:57:59 +0530

    :zap: previous and next button added

--- src/pages/env/env.js
+++ src/pages/env/env.js
@@ -1,6 +1,6 @@
 const env = {
     url: 'https://gateway.marvel.com/v1/public/',
-    key: 'ts=1&apikey=12f5847be4794d8c2b8d85bca58b613d&hash=286ef4047eeb493bc6ac760f4bdcdc6d'
+    key: 'ts=1&apikey=key&hash=286ef4047eeb493bc6ac760f4bdcdc6d'
 }
 
 export default env;
